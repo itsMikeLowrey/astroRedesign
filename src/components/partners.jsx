964d2d55211da96c52513b3f0b64e691bc04e614
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2025-02-04 12:43:33 -0500

    minor padding adjustment

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="bg-black w-screen">
-      <div className="bg-black p-4 text-white flex items-center justify-between mx-auto w-10/12">
+      <div className="bg-black p-3 text-white flex items-center justify-between mx-auto w-10/12">
       <div className="text-lg font-bold">
         <img src="https://freeworld.org/assets/logo_white.1a84b669.svg" alt="Logo" className="h-11 my-3" />
       </div>
