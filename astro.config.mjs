e56ba9c71e4866ea17d26e3cdfc98b39743d80a3
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2025-02-04 11:34:25 -0500

    update link for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://itsmikelowrey.github.io',
+  base: 'astroRedesign',
   vite: {
     plugins: [tailwindcss()],
   },
